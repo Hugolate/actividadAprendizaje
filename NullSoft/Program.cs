8d3b1962cb6719381789f5b1ed2770b12bd3b71e
Ricardo Ros <103067416+Potoyd@users.noreply.github.com>
2022-11-16 20:12:55 +0100

    Loop finished

--- NullSoft/Program.cs
+++ NullSoft/Program.cs
@@ -9,79 +9,156 @@ namespace NullSoft
         {
             Console.WriteLine("This is PlaySoft");
             Thread.Sleep(2000);
-            Console.WriteLine("Menu: ");
-            Console.WriteLine("1. Sign Up");
-            Console.WriteLine("2. Sign In");
-            //Console.WriteLine("3. Search Songs");
-            Console.WriteLine("3. Exit");
-            int opcion = 0;
-            while (opcion < 1 || opcion > 3)
+            Boolean bucle = false;
+            while (!bucle)
             {
-                opcion = Convert.ToInt32(Console.ReadLine());
-                if (opcion < 1 || opcion > 3)
+                Console.WriteLine("Menu: ");
+                Console.WriteLine("1. Sign Up");
+                Console.WriteLine("2. Sign In");
+                //Console.WriteLine("3. Search Songs");
+                Console.WriteLine("3. Exit");
+                int opcion = 0;
+                while (opcion < 1 || opcion > 3)
                 {
-                    Console.WriteLine("Invalid option");
-                }
-            }
-            switch (opcion)
-            {
-                case 1:
-                    Console.WriteLine(" _________________");
-                    Console.WriteLine("|  Register menu  |");
-                    Console.WriteLine("|-----------------|");
-                    Console.WriteLine("|    Username     |");
-                    Console.WriteLine("|    ________     |");
-                    Console.WriteLine("|                 |");
-                    Console.WriteLine("|    Password     |");
-                    Console.WriteLine("|    ________     |");
-                    Console.WriteLine("|_________________|");
-                    Console.WriteLine("Write your username: ");
-                    String username = Console.ReadLine();
-                    Console.WriteLine(" _________________ ");
-                    Console.WriteLine("|  Register menu  |");
-                    Console.WriteLine("|-----------------|");
-                    Console.WriteLine("|    Username     |");
-                    Console.WriteLine($"|    {username.PadRight(13)}|");
-                    Console.WriteLine("|                 |");
-                    Console.WriteLine("|    Password     |");
-                    Console.WriteLine("|    ________     |");
-                    Console.WriteLine("|_________________|");
-                    Console.WriteLine("Write your password: ");
-                    var password = string.Empty;
-                    ConsoleKey key;
-                    do
+                    opcion = Convert.ToInt32(Console.ReadLine());
+                    if (opcion < 1 || opcion > 3)
                     {
-                        var keyInfo = Console.ReadKey(intercept: true);
-                        key = keyInfo.Key;
+                        Console.WriteLine("Invalid option");
+                    }
+                }
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine(" _________________");
+                        Console.WriteLine("|  Register menu  |");
+                        Console.WriteLine("|-----------------|");
+                        Console.WriteLine("|    Username     |");
+                        Console.WriteLine("|    ________     |");
+                        Console.WriteLine("|                 |");
+                        Console.WriteLine("|    Password     |");
+                        Console.WriteLine("|    ________     |");
+                        Console.WriteLine("|_________________|");
+                        Console.WriteLine("Write your username: ");
+                        String username = Console.ReadLine();
+                        Console.WriteLine(" _________________ ");
+                        Console.WriteLine("|  Register menu  |");
+                        Console.WriteLine("|-----------------|");
+                        Console.WriteLine("|    Username     |");
+                        Console.WriteLine($"|    {username.PadRight(13)}|");
+                        Console.WriteLine("|                 |");
+                        Console.WriteLine("|    Password     |");
+                        Console.WriteLine("|    ________     |");
+                        Console.WriteLine("|_________________|");
+                        Console.WriteLine("Write your password: ");
+                        var password = string.Empty;
+                        ConsoleKey key;
+                        do
+                        {
+                            var keyInfo = Console.ReadKey(intercept: true);
+                            key = keyInfo.Key;
 
-                        if (key == ConsoleKey.Backspace && password.Length > 0)
+                            if (key == ConsoleKey.Backspace && password.Length > 0)
+                            {
+                                Console.Write("\b \b");
+                                password = password[0..^1];
+                            }
+                            else if (!char.IsControl(keyInfo.KeyChar))
+                            {
+                                Console.Write("*");
+                                password += keyInfo.KeyChar;
+                            }
+                        } while (key != ConsoleKey.Enter);
+                        Console.WriteLine(" ");
+                        Console.WriteLine(" _________________ ");
+                        Console.WriteLine("|  Register menu  |");
+                        Console.WriteLine("|-----------------|");
+                        Console.WriteLine("|    Username     |");
+                        Console.WriteLine($"|    {username.PadRight(13)}|");
+                        Console.WriteLine("|                 |");
+                        Console.WriteLine("|    Password     |");
+                        Console.WriteLine("|    ********     |");
+                        Console.WriteLine("|_________________|");
+                        Console.WriteLine($"Account created for {username}");
+                        break;
+                    case 2:
+                        Console.WriteLine("Username: ");
+                        username = Console.ReadLine();
+                        Console.WriteLine("Password: ");
+                        password = string.Empty;
+                        do
                         {
-                            Console.Write("\b \b");
-                            password = password[0..^1];
-                        }
-                        else if (!char.IsControl(keyInfo.KeyChar))
+                            var keyInfo = Console.ReadKey(intercept: true);
+                            key = keyInfo.Key;
+
+                            if (key == ConsoleKey.Backspace && password.Length > 0)
+                            {
+                                Console.Write("\b \b");
+                                password = password[0..^1];
+                            }
+                            else if (!char.IsControl(keyInfo.KeyChar))
+                            {
+                                Console.Write("*");
+                                password += keyInfo.KeyChar;
+                            }
+                        } while (key != ConsoleKey.Enter);
+
+                        Console.WriteLine(" ");
+                        Console.WriteLine(" _________________");
+                        Console.WriteLine("|  Register menu  |");
+                        Console.WriteLine("|-----------------|");
+                        Console.WriteLine("|    Username     |");
+                        Console.WriteLine($"|    {username.PadRight(13)}|");
+                        Thread.Sleep(1500);
+                        Console.WriteLine("|                 |");
+                        Console.WriteLine("|    Password     |");
+                        Console.WriteLine("|    ********     |");
+                        Thread.Sleep(1500);
+                        Console.WriteLine("|_________________|");
+                        Console.WriteLine($"Welcome, {username}");
+
+                        //If VALIDARLOGIN = true
+                        Boolean bucleUser = false;
+                        while (!bucleUser)
                         {
-                            Console.Write("*");
-                            password += keyInfo.KeyChar;
+                            Console.WriteLine("Menu: ");
+                            Console.WriteLine("1. Search song");
+                            Console.WriteLine("2. Create playlist");
+                            Console.WriteLine("3. Remove playlist");
+                            Console.WriteLine("3. Search playlist");
+                            Console.WriteLine("4. View playlist");
+                            Console.WriteLine("5. Exit");
+
+                            opcion = 0;
+                            while (opcion < 1 || opcion > 5)
+                            {
+                                opcion = Convert.ToInt32(Console.ReadLine());
+                                if (opcion < 1 || opcion > 5)
+                                {
+                                    Console.WriteLine("Invalid option");
+                                }
+                            }
+                            switch (opcion)
+                            {
+                                case 1:
+                                    break;
+                                case 2:
+                                    break;
+                                case 3:
+                                    break;
+                                case 4:
+                                    break;
+                                case 5:
+                                    bucle = true;
+                                    break;
+                            }
                         }
-                    } while (key != ConsoleKey.Enter);
-                    Console.WriteLine(" ");
-                    Console.WriteLine(" _________________ ");
-                    Console.WriteLine("|  Register menu  |");
-                    Console.WriteLine("|-----------------|");
-                    Console.WriteLine("|    Username     |");
-                    Console.WriteLine($"|    {username.PadRight(13)}|");
-                    Console.WriteLine("|                 |");
-                    Console.WriteLine("|    Password     |");
-                    Console.WriteLine("|    ********     |");
-                    Console.WriteLine("|_________________|");
-                    break;
-                case 2:
-                    Console.WriteLine("Opcion 2");
-                    break;
-                case 3:
-                    Console.WriteLine("Opcion 3");
-                    break;
+
+                        break;
+                    case 3:
+                        Console.WriteLine("Stay soft");
+                        bucle = true;
+                        break;
+                }
             }
         }
     }
